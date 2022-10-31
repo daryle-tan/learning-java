917487a664c011af772cf61d707bedf908cbfbc2
daryle <daryle.tan@gmail.com>
2022-10-30 21:35:33 -0500

    compiled

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hello World! It's me Daryle!");
+        System.out.println("Hello World! It's me Daryle! and I love TACOS!");
     }
 }
