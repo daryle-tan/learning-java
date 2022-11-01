74cae04985c690bb96952433e06c201f2384c349
daryle <daryle.tan@gmail.com>
2022-10-31 23:12:15 -0500

    fin ch 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hello World! It's me Daryle! and I love TACOS!");
+        // System.out.println("Hello World! It's me Daryle! and I love TACOS!");
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        
+        Boolean hasPerfectAttendance = true;
+        
+        String studentName = "Kayla Hammon";
+        char studentFirstInitial = studentName.charAt(0);
+        char studentLastInitial = studentName.charAt(6);
+        
+
+        System.out.println(studentName + " has a GPA of " + studentGPA );
+        System.out.println("What do you want to update it to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+
+        System.out.println(studentName + " now has a GPA of " + studentGPA);
     }
 }
