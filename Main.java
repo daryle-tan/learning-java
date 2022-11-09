b7912a6cb1cae653c96956e1a6fcfbac8f49f433
daryle <daryle.tan@gmail.com>
2022-11-09 10:17:15 -0600

    fin

--- Main.java
+++ Main.java
@@ -1,12 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
-        Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
+        StudentProfile studentA = new StudentProfile("Dee", "Tee", 2022, 3.08, "Business Management");
+        StudentProfile studentB = new StudentProfile("Homer", "Simpson", 2023, 3.6, "Computer Sciences");
+        
+        studentA.incrementExpectedGraduationYear();
+        System.out.println("This is " + studentA.firstName + " " + studentA.lastName + " graduation year: " + studentA.expectedYearToGraduate);
+        // Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
+        // Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
 
-        double triangleAArea = triangleA.findArea();
-        System.out.println(triangleAArea);
+        // double triangleAArea = triangleA.findArea();
+        // System.out.println(triangleAArea);
 
-        double triangleBArea = triangleB.findArea();
-        System.out.println(triangleBArea);
+        // double triangleBArea = triangleB.findArea();
+        // System.out.println(triangleBArea);
+
+        // System.out.println(triangleA.sideLenThree);
+        // System.out.println(triangleB.base);
+
+        // System.out.println(Triangle.numOfSides);
     }
 }
---
+++ StudentProfile.java
@@ -0,0 +1,19 @@
+public class StudentProfile {
+    String firstName;
+    String lastName;
+    int expectedYearToGraduate;
+    double GPA;
+    String declaredMajor;
+
+    public StudentProfile (String firstName, String lastName, int expectedYearToGraduate, double GPA, String declaredMajor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.expectedYearToGraduate = expectedYearToGraduate;
+        this.GPA = GPA;
+        this.declaredMajor = declaredMajor;
+    }
+
+    public void incrementExpectedGraduationYear() {
+         this.expectedYearToGraduate = this.expectedYearToGraduate + 1;
+    }
+}
